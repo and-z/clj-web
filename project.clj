e0269bc93eb4f90623aa59f5b0e30e7515bfc5ed
Andreas Zimpel <mail4andz@gmail.com>
2020-06-13 10:51:14 +0200

    Add figwheel

--- project.clj
+++ project.clj
@@ -3,5 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :aliases
+  {"fig" ["trampoline" "run" "-m" "figwheel.main"]}
+
   :dependencies [[org.clojure/clojure "1.10.1"]]
+
+  :profiles
+  {:dev {:dependencies [[org.clojure/clojurescript "1.10.773"]
+                        [com.bhauman/figwheel-main "0.2.8"]
+                        [com.bhauman/rebel-readline-cljs "0.1.4"]]}}
+
+
   )

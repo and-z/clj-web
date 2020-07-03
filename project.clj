983b567d3ad678dfaffed7f91051106643db741a
Andreas Zimpel <mail4andz@gmail.com>
2020-07-03 22:57:45 +0200

    Explicitely configure target as figwheel resources path

--- project.clj
+++ project.clj
@@ -19,6 +19,7 @@
   {:backend {:source-paths ["src/clj"]}
 
    :fig {:source-paths ["src/cljs"]
+         :resource-paths ["target"]
          :dependencies [[org.clojure/clojurescript "1.10.773"]
                         [com.bhauman/figwheel-main "0.2.9"]
                         [com.bhauman/rebel-readline-cljs "0.1.4"]]}})

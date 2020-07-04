c1d6f0e9bdc60fc83b215a20f40d413869de18b6
Andreas Zimpel <mail4andz@gmail.com>
2020-07-04 23:12:24 +0200

    Set default figwheel build

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,4 @@
 ((clojure-mode . ((cider-lein-global-options . "with-profile +backend")))
- (clojurescript-mode . ((cider-lein-global-options . "with-profile fig"))))
+ (clojurescript-mode . ((cider-lein-global-options . "with-profile fig")
+                        (cider-default-cljs-repl . figwheel-main)
+                        (cider-figwheel-main-default-options . ":dev"))))
